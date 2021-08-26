1914b71647ec0afc0d5a3e225634d77c1a950397
Noye Samuel <samule.noye@amalitech.org>
2021-08-26 22:32:53 +0000

    file path

--- routes/admin.js
+++ routes/admin.js
@@ -27,7 +27,7 @@ router.use(flash());
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
-    destination: '../img/',
+    destination: './img/',
     filename: function(req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
